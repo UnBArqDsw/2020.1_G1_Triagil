fdd2a284fb561cc066ba61b04740a10495e25ffb
vitorAlves7 <vitor.alves07750@outlook.com>
2020-11-18 07:56:15 -0300

    Adicionando estrutura de escolha de enfermidade
    Signed-off-by: vitorAlves7 <vitor.alves07750@outlook.com>
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

--- Back/src/app/controller/TriageRecordController.js
+++ Back/src/app/controller/TriageRecordController.js
@@ -3,21 +3,34 @@ import TriageRecord from '../models/TriageRecord';
 class TriageRecordController {
   async store(req, res) {
 
-    let teste = req.body;
 
+    let teste = req.body;
     let vetorExemplo = [];
-    vetorExemplo.push(teste.headacheSigns);
+
+    if(teste.headache){
+      vetorExemplo.push(teste.headacheSigns);
+      delete teste.headacheSigns;
+    } else if(teste.soreThroat){
+        vetorExemplo.push(teste.soreThroatSigns);
+        delete teste.soreThroatSigns;
+    } else if(teste.convulsion){
+        vetorExemplo.push(teste.convulsionSigns);
+        delete teste.convulsionSigns;
+    } else if(teste.fainting){
+        vetorExemplo.push(teste.faintingSigns);
+        delete teste.faintingSigns;
+    } else if(teste.diarrhea){
+        vetorExemplo.push(teste.diarrheaSigns);
+        delete teste.diarrheaSigns;
+    }
 
       return res.json({
+        teste,
         vetorExemplo
     });
   }
 
-
   async show(req,res) {
-
-
-
     return res.json({
         triageRecord
     });
