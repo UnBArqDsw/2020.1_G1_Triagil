8c458ba1bdeeb00a8a99cde56014b13a9739ed21
vitorAlves7 <vitor.alves07750@outlook.com>
2020-11-18 09:25:47 -0300

    Adicinando objeto para manipular as enfermidades
    Signed-off-by: vitorAlves7 <vitor.alves07750@outlook.com>
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

--- Back/src/app/controller/TriageRecordController.js
+++ Back/src/app/controller/TriageRecordController.js
@@ -5,28 +5,53 @@ class TriageRecordController {
 
 
     let teste = req.body;
-    let vetorExemplo = [];
+    let  objectExemplo = {};
 
     if(teste.headache){
-      vetorExemplo.push(teste.headacheSigns);
-      delete teste.headacheSigns;
-    } else if(teste.soreThroat){
-        vetorExemplo.push(teste.soreThroatSigns);
-        delete teste.soreThroatSigns;
-    } else if(teste.convulsion){
-        vetorExemplo.push(teste.convulsionSigns);
-        delete teste.convulsionSigns;
-    } else if(teste.fainting){
-        vetorExemplo.push(teste.faintingSigns);
-        delete teste.faintingSigns;
-    } else if(teste.diarrhea){
-        vetorExemplo.push(teste.diarrheaSigns);
-        delete teste.diarrheaSigns;
+
+      objectExemplo.estadoMentalAlterado = teste.estadoMentalAlterado;
+      objectExemplo.estadoMentalAlteradoNotes = teste.estadoMentalAlteradoNotes;
+      objectExemplo.sinaisDeChoque_headache = teste.sinaisDeChoque_headache;
+      objectExemplo.sinaisDeChoqueNotes_headache = teste.sinaisDeChoqueNotes_headache;
+      objectExemplo.convulsion_headache = teste.convulsion_headache;
+      objectExemplo.sinaisNeurologicosFocais_headache = teste.sinaisNeurologicosFocais_headache;
+      objectExemplo.sinaisNeurologicosFocaisNotes_headache = teste.sinaisNeurologicosFocaisNotes_headache;
+      objectExemplo.aumentoSubitoDaDor = teste.aumentoSubitoDaDor;
+      objectExemplo.aumentoSubitoDaDorNotes = teste.aumentoSubitoDaDorNotes;
+      objectExemplo.perdaAgudaCompletaVisao = teste.perdaAgudaCompletaVisao;
+      objectExemplo.menigismo_headache = teste.menigismo_headache;
+      objectExemplo.menigismoNotes_headache = teste.menigismoNotes_headache;
+      objectExemplo.pain_headache = teste.pain_headache;
+      objectExemplo.temperature_headache = teste.temperature_headache;
+      objectExemplo.inconsciencia_headache = teste.inconsciencia_headache;
+      objectExemplo.inconscienciaNotes_headache = teste.inconscienciaNotes_headache;
+      objectExemplo.diminuicaoVisao = teste.diminuicaoVisao;
+      objectExemplo.nauseaEVomito = teste.nauseaEVomito;
+
+      // if(objectExemplo.estadoMentalAlterado || objectExemplo.sinaisDeChoque_headache
+      //    || objectExemplo.convulsion_headache || objectExemplo.sinaisNeurologicosFocais_headache ||
+      //    objectExemplo.aumentoSubitoDaDor){
+      //     teste.manchesterPriority = 5;
+      // }
+
     }
+    // else if(teste.soreThroat){
+    //     vetorExemplo.push(teste.soreThroatSigns);
+    //     delete teste.soreThroatSigns;
+    // } else if(teste.convulsion){
+    //     vetorExemplo.push(teste.convulsionSigns);
+    //     delete teste.convulsionSigns;
+    // } else if(teste.fainting){
+    //     vetorExemplo.push(teste.faintingSigns);
+    //     delete teste.faintingSigns;
+    // } else if(teste.diarrhea){
+    //     vetorExemplo.push(teste.diarrheaSigns);
+    //     delete teste.diarrheaSigns;
+    // }
 
       return res.json({
-        teste,
-        vetorExemplo
+        objectExemplo,
+        teste
     });
   }
 
