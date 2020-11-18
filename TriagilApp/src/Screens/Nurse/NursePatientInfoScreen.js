90431a10b01ca398c9b380ca4ffec09a0de87a71
Miguel-Alves <miguelhenriquealvesdeoliveira@gmail.com>
2020-11-18 20:30:29 -0300

    Recebendo informações dinâmicas
    Co-authored-by: Lude Ribeiro <ludeyuridrums@gmail.com>
    Co-authored-by: deborapvilela <deborapvilela@gmail.com>
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- TriagilApp/src/Screens/Nurse/NursePatientInfoScreen.js
+++ TriagilApp/src/Screens/Nurse/NursePatientInfoScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 
 import Button from '../../components/Button';
@@ -9,6 +9,12 @@ import TriTextInput from '../../components/TriTextInput';
 import { LinearGradient } from "expo-linear-gradient";
 
 import PasswordIcon from '../../Icons/key.png';
+import userIcon from '../../Icons/user.png';
+import cpfIcon from '../../Icons/CPF.png';
+import genderIcon from '../../Icons/gender.png';
+import calendarIcon from '../../Icons/calendar.png';
+
+
 
 const styles = StyleSheet.create({
     SuperiorOption: {
@@ -32,7 +38,7 @@ const styles = StyleSheet.create({
         borderBottomWidth: 0,
         borderRadius: 2,
         marginLeft: 10,
-        width: '60%',
+        width: '80%',
         
     },
     bottom: {
@@ -60,9 +66,9 @@ class NursePatientInfoScreen extends React.Component {
     };
 
     
-    
     render () {
-        
+        const patient = this.props.route.params.patient;
+        //console.log('AQUI OS PARAMETROS: ', this.props.route.params);
         
         return (
             <LinearGradient style={{flex: 1,flexGrow: 1, justifyContent: 'center'}} colors={['rgba(1,50,126,1)', 'rgba(88,200,245,1)']}>
@@ -78,26 +84,29 @@ class NursePatientInfoScreen extends React.Component {
                     
                     <View style={styles.midle}>
                         <View style={styles.patientName}>
-                            <Text style={{fontSize: 26, marginLeft: 10, color:'#FAFAFA'}}>Nome x</Text>
+                            <Text style={{fontSize: 26, marginLeft: 10, color:'#FAFAFA'}}>Paciente: {patient.name}</Text>
                         </View>
                         <ScrollView style={{ borderTopColor: '#1BC47D', borderTopWidth: 5}}
                             contentContainerStyle={{ alignItems: 'center'}}
                         >
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Senha do Hospital'} icon={PasswordIcon}/>
-
+                            <TriTextInput editable={false} placeholder={'Nome do Pai: ' + patient.father_name} icon={userIcon}/>
+                            <TriTextInput editable={false} placeholder={'Nome do Mãe: ' + patient.mother_name} icon={userIcon}/>
+                            <TriTextInput editable={false} placeholder={'CPF: ' + patient.cpf} icon={cpfIcon}/>
+                            <TriTextInput editable={false} placeholder={'RG: ' + patient.rg} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'IDADE: ' + patient.age + ' ANOS'} icon={calendarIcon}/>
+                            <TriTextInput editable={false} placeholder={'Nascimento: ' + patient.birth} icon={calendarIcon}/>
+                            <TriTextInput editable={false} placeholder={'Tipo Sanguíneo: ' + patient.blood_type} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Sexo: ' + patient.sex} icon={genderIcon}/>
+                            <TriTextInput editable={false} placeholder={'Religião: ' + patient.religions} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Raça: ' + patient.race} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Estado Civil: ' + patient.state_civil} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Nacionalidade: ' + patient.nationality} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Naturalidade: ' + patient.naturalness} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Escolaridade: ' + patient.schooling} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Profissão: ' + patient.professions} icon={PasswordIcon}/>
                         </ScrollView>
                     </View>
                     
-                        
-
                     <View style={styles.bottom}>
                         <Button label={'VOLTAR'} width={'35%'} labelColor={'#FAFAFA'} color={'#FB0C0D'} />
                         <Button label={'INICIAR TRIAGEM'} width={'55%'} labelColor={'#FAFAFA'} color={'#1BC47D'} />
