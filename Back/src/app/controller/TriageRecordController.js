05d22568b027eaf1b82a7d7f25172e5e039ba918
FranciscoHeronildo <heronildo.2010@hotmail.com>
2020-11-19 12:11:34 -0300

    Adicionando lógica da triagem da convulsao
    Co-authored-by: Miguel-Alves <miguelhenriquealvesdeoliveira@gmail.com>
    Co-authored-by: deborapvilela <deborapvilela@gmail.com>
    Co-authored-by: Lude Ribeiro <ludeyuridrums@gmail.com>
    Co-authored-by: Vitor Alves <vitor.alves07750@outlook.com>
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- Back/src/app/controller/TriageRecordController.js
+++ Back/src/app/controller/TriageRecordController.js
@@ -11,19 +11,20 @@ class TriageRecordController {
       if(triageRecord.estadomental_alterado || triageRecord.sinais_de_choque_headache
          || triageRecord.convulsion_headache || triageRecord.sinais_neurologicos_focais_headache ||
          triageRecord.aumento_subitodador){
-          triageRecord.manchester_priority = 5;
+           triageRecord.manchester_priority = 5;
       } else if(triageRecord.perda_aguda_completa_visao ||  triageRecord.menigismo_headache || triageRecord.pain_headache >= 7){
         triageRecord.manchester_priority = 4;
-      } else if((triageRecord.temperature_headache >= 35.00 && triageRecord.temperature_headache <= 37.8) || triageRecord.inconsciencia_headach ||
-          triageRecord.diminuicao_visao || (triageRecord.pain_headache >= 4 && triageRecord.pain_headache <= 6) ||
-          triageRecord.nausea_e_vomito){
-            triageRecord.manchester_priority = 3;
+      } else if((triageRecord.temperature_headache >= 35.00 && triageRecord.temperature_headache <= 37.8) || 
+      triageRecord.inconsciencia_headach || triageRecord.diminuicao_visao || 
+      (triageRecord.pain_headache >= 4 && triageRecord.pain_headache <= 6) || triageRecord.nausea_e_vomito){
+        triageRecord.manchester_priority = 3;
       } else if(triageRecord.pain_headache >= 1 && triageRecord.pain_headache <= 3){
-            triageRecord.manchester_priority = 2;
+        triageRecord.manchester_priority = 2;
       } else {
-            triageRecord.manchester_priority = 1;
+        triageRecord.manchester_priority = 1;
       }
     }
+
     if(triageRecord. sore_throat){
 
       if(triageRecord.comprometimento_vias_aereas_soathroat ){
@@ -41,6 +42,24 @@ class TriageRecordController {
       }
 
     }
+
+    if (triageRecord.convulsion){
+
+      if(triageRecord.comprometimentoViasAereas || 
+        (triageRecord.alteracoesGlicemicasNumber < 70 && triageRecord.alteracoesGlicemicasNumber > 250) || 
+        triageRecord.respiracaoInadequada || triageRecord.sinaisNeurologicosFocais 
+        || triageRecord.criseConvulsivaReentrante || triageRecord.menigismo) {
+        triageRecord.manchester_priority = 5;
+      } else if(triageRecord.overdoseEnvenenamento || triageRecord.traumaCranianoRecente) {
+        triageRecord.manchester_priority = 4;
+      } else if(triageRecord.posComicial || triageRecord.epilepsiaTratada || triageREcord.criseConvulsivaRecente){
+        triageRecord.manchester_priority = 3;
+      } else {
+        triageRecord.manchester_priority = 2;
+      }
+
+    }
+
     const record = await TriageRecord.create(triageRecord);
 
       return res.json({
