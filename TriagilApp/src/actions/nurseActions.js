20320e2cba4b65a935ab759dde29c552a574770a
Miguel-Alves <miguelhenriquealvesdeoliveira@gmail.com>
2020-11-18 23:24:32 -0300

    Criando a action de alteração da senha da enfermeira
    Co-authored-by: Lude Ribeiro <ludeyuridrums@gmail.com>
    Co-authored-by: deborapvilela <deborapvilela@gmail.com>
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- TriagilApp/src/actions/nurseActions.js
+++ TriagilApp/src/actions/nurseActions.js
@@ -1,6 +1,11 @@
-import { ADD_NURSE_INFO, } from './types';
+import { ADD_NURSE_INFO, UPDATE_NURSE_PASSWORD } from './types';
   
 export const addNurseInfo = (nurseInfo) => ({
     type: ADD_NURSE_INFO,
+    payload: nurseInfo,
+});
+
+export const updateNursePassword = (nurseInfo) => ({
+    type: UPDATE_NURSE_PASSWORD,
     payload: nurseInfo,
 });
