750df2962e5dcfd7b9575fa607691974413cef3b
Miguel-Alves <miguelhenriquealvesdeoliveira@gmail.com>
2020-11-19 16:55:05 -0300

    Corrigindo icones da tela de informações sobre paciente
    Co-authored-by: Lude Ribeiro <ludeyuridrums@gmail.com>
    Co-authored-by: deborapvilela <deborapvilela@gmail.com>
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- TriagilApp/src/Screens/Nurse/NursePatientInfoScreen.js
+++ TriagilApp/src/Screens/Nurse/NursePatientInfoScreen.js
@@ -15,7 +15,14 @@ import userIcon from '../../Icons/user.png';
 import cpfIcon from '../../Icons/CPF.png';
 import genderIcon from '../../Icons/gender.png';
 import calendarIcon from '../../Icons/calendar.png';
-
+import rgIcon from '../../Icons/rg.png';
+import bloodIcon from '../../Icons/blood-type.png';
+import religionIcon from '../../Icons/pray.png';
+import civilStateIcon from '../../Icons/marital_status.png';
+import nationalityIcon from '../../Icons/naturalization.png';
+import naturalityIcon from '../../Icons/flag.png';
+import schoolingIcon from '../../Icons/school.png';
+import professionsIcon from '../../Icons/suitcase.png';
 
 
 const styles = StyleSheet.create({
@@ -110,18 +117,18 @@ class NursePatientInfoScreen extends React.Component {
                             <TriTextInput editable={false} placeholder={'Nome do Pai: ' + patient.father_name} icon={userIcon}/>
                             <TriTextInput editable={false} placeholder={'Nome do Mãe: ' + patient.mother_name} icon={userIcon}/>
                             <TriTextInput editable={false} placeholder={'CPF: ' + patient.cpf} icon={cpfIcon}/>
-                            <TriTextInput editable={false} placeholder={'RG: ' + patient.rg} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'RG: ' + patient.rg} icon={rgIcon}/>
                             <TriTextInput editable={false} placeholder={'IDADE: ' + patient.age + ' ANOS'} icon={calendarIcon}/>
                             <TriTextInput editable={false} placeholder={'Nascimento: ' + patient.birth} icon={calendarIcon}/>
-                            <TriTextInput editable={false} placeholder={'Tipo Sanguíneo: ' + patient.blood_type} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Tipo Sanguíneo: ' + patient.blood_type} icon={bloodIcon}/>
                             <TriTextInput editable={false} placeholder={'Sexo: ' + patient.sex} icon={genderIcon}/>
-                            <TriTextInput editable={false} placeholder={'Religião: ' + patient.religions} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Raça: ' + patient.race} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Estado Civil: ' + patient.state_civil} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Nacionalidade: ' + patient.nationality} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Naturalidade: ' + patient.naturalness} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Escolaridade: ' + patient.schooling} icon={PasswordIcon}/>
-                            <TriTextInput editable={false} placeholder={'Profissão: ' + patient.professions} icon={PasswordIcon}/>
+                            <TriTextInput editable={false} placeholder={'Religião: ' + patient.religions} icon={religionIcon}/>
+                            <TriTextInput editable={false} placeholder={'Raça: ' + patient.race} icon={userIcon}/>
+                            <TriTextInput editable={false} placeholder={'Estado Civil: ' + patient.state_civil} icon={civilStateIcon}/>
+                            <TriTextInput editable={false} placeholder={'Nacionalidade: ' + patient.nationality} icon={nationalityIcon}/>
+                            <TriTextInput editable={false} placeholder={'Naturalidade: ' + patient.naturalness} icon={naturalityIcon}/>
+                            <TriTextInput editable={false} placeholder={'Escolaridade: ' + patient.schooling} icon={schoolingIcon}/>
+                            <TriTextInput editable={false} placeholder={'Profissão: ' + patient.professions} icon={professionsIcon}/>
                         </ScrollView>
                     </View>
                     
