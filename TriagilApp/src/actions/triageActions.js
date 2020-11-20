4807729a3b756775ddaf97044a84c4ae056b3a99
Miguel-Alves <miguelhenriquealvesdeoliveira@gmail.com>
2020-11-20 06:01:38 -0300

    Criando actions de triagem
    Co-authored-by: Lude Ribeiro <ludeyuridrums@gmail.com>
    Co-authored-by: deborapvilela <deborapvilela@gmail.com>
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- TriagilApp/src/actions/triageActions.js
+++ TriagilApp/src/actions/triageActions.js
@@ -1,6 +1,32 @@
-import { RESET_TRIAGE } from './types';
+import { RESET_TRIAGE, HEADACHE_TRIAGE, SORETHROAT_TRIAGE, DIARRHEA_TRIAGE, 
+FAINTING_TRIAGE, CONVULSION_TRIAGE } from './types';
 
 
 export const resetTriage = () => ({
     type: RESET_TRIAGE,
-})
+});
+
+export const headacheTriage = (headacheInfo) => ({
+    type: HEADACHE_TRIAGE,
+    payload: headacheInfo,
+});
+
+export const sorethroatTriage = (sorethroatInfo) => ({
+    type: SORETHROAT_TRIAGE,
+    payload: sorethroatInfo,
+});
+
+export const diarrheaTriage = (diarrheaInfo) => ({
+    type: DIARRHEA_TRIAGE,
+    payload: diarrheaInfo,
+});
+
+export const faintingTriage = (faintingInfo) => ({
+    type: FAINTING_TRIAGE,
+    payload: faintingInfo,
+});
+
+export const convulsionTriage = (convulsionInfo) => ({
+    type: CONVULSION_TRIAGE,
+    payload: convulsionInfo,
+});
